ee97bc450d18efef67347a54de5fe5c7e4a47483
ntg-santo <ntgsanto@gmail.com>
2025-06-18 12:17:03 +0600

    update

---
+++ FAL_Commercial_1009_ip_server_start.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "D:\NTG_App\www\Fashion_Asia_Limited\FAL_Commercial"
+
+start /B cmd /C "php artisan serve --host=192.168.100.231:1009"
+start /B cmd /C "php artisan ser"
--- ip_server_start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "D:\NTG_APP\www\worker_matrix"
-
-start /B cmd /C "php artisan serve --host=192.168.100.233:1000"
-start /B cmd /C "php artisan ser"
